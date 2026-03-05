c1d550dcfb86253a2868e73edd7a9c87a8b72cdb
ashish <ashishmurkar23@gmail.com>
2026-03-05 21:13:06 +0530

    add terraform and provider files

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  region           = "us-ashburn-1"
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaagsgs3dyg4jrtn4jjnb33dqcbqfijfjq64vd2gaubi5j3cosg7uoq"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaaiipnpoaammbylf2h36lcucgufelsiexbeunmxh4hjke64zojlqwa"
+  fingerprint      = "1e:de:2c:3e:b9:4a:c1:11:c6:40:0e:fa:8b:6b:c3:59"
+  private_key_path = "C:/oci-keys/pvt.pem"
+}
